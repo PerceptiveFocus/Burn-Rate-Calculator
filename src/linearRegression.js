0fe58d463cdec6320378b0b6d80a26f5f5f8980a
Perceptive Focus <113016229+PerceptiveFocus@users.noreply.github.com>
2023-03-20 08:24:09 -0700

    Update linearRegression.js

--- src/linearRegression.js
+++ src/linearRegression.js
@@ -1,1 +1,23 @@
+export function calculateLinearRegression(xValues, yValues) {
+  const n = xValues.length;
+  let sumX = 0;
+  let sumY = 0;
+  let sumXY = 0;
+  let sumXX = 0;
+  let sumYY = 0;
+
+  for (let i = 0; i < n; i++) {
+    sumX += xValues[i];
+    sumY += yValues[i];
+    sumXY += xValues[i] * yValues[i];
+    sumXX += xValues[i] * xValues[i];
+    sumYY += yValues[i] * yValues[i];
+  }
+
+  const slope = (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX);
+  const intercept = (sumY - slope * sumX) / n;
+  const rSquared = Math.pow((n * sumXY - sumX * sumY) / Math.sqrt((n * sumXX - sumX * sumX) * (n * sumYY - sumY * sumY)), 2);
+
+  return { slope, intercept, rSquared };
+}
 
