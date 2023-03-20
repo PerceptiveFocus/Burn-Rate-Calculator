922d5e5928abb720bff9d8bd182e825d7c15239c
Perceptive Focus <113016229+PerceptiveFocus@users.noreply.github.com>
2023-03-19 22:05:35 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BurnRateProjection />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
