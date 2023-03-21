56d1b1eab38040c03f73f9afe3a6ae306b9160e6
Perceptive Focus <113016229+PerceptiveFocus@users.noreply.github.com>
2023-03-21 02:27:34 -0700

    Update BurnRateProjection.js

--- src/BurnRateProjection.js
+++ src/BurnRateProjection.js
@@ -2,23 +2,37 @@ import React, { useState, useRef, useEffect } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
 import Chart from 'chart.js/auto';
 
-const useBurnRateChart = (initialBalance, expenses, quarters) => {
+const useBurnRateChart = (initialBalance, expenses, period, periodType) => {
   const chartRef = useRef(null);
   const canvasRef = useRef(null);
 
   useEffect(() => {
     updateChart();
-  }, [initialBalance, expenses, quarters]);
+  }, [initialBalance, expenses, period, periodType]);
 
-  const calculateBurnRateData = (initialBalance, expenses, quarters) => {
+  const calculateBurnRateData = (initialBalance, expenses, period, periodType) => {
+    let quarters;
+    if (periodType === 'quarters') {
+      quarters = period;
+    } else {
+      quarters = period * 4;
+    }
+
     const cashBalance = [initialBalance];
     for (let i = 1; i < quarters; i++) {
       cashBalance.push(cashBalance[i - 1] - expenses);
     }
-    const burnRate = cashBalance.map((value) => value - expenses);
+    const burnRate = cashBalance.map((value, index) => value - (expenses * index));
+
+    let xAxisLabel;
+    if (periodType === 'quarters') {
+      xAxisLabel = 'Quarter';
+    } else {
+      xAxisLabel = 'Year';
+    }
 
     const chartData = {
-      labels: [...Array(quarters).keys()].map((x) => x + 1),
+      labels: [...Array(quarters).keys()].map((x) => (periodType === 'quarters' ? x + 1 : Math.floor((x + 1) / 4))),
       datasets: [
         {
           label: 'Burn Rate Projection',
@@ -35,7 +49,7 @@ const useBurnRateChart = (initialBalance, expenses, quarters) => {
         x: {
           title: {
             display: true,
-            text: 'Quarter',
+            text: xAxisLabel,
           },
         },
         y: {
@@ -55,7 +69,7 @@ const useBurnRateChart = (initialBalance, expenses, quarters) => {
       chartRef.current.destroy();
     }
 
-    const burnRateData = calculateBurnRateData(initialBalance, expenses, quarters);
+    const burnRateData = calculateBurnRateData(initialBalance, expenses, period, periodType);
     const ctx = canvasRef.current.getContext('2d');
 
     chartRef.current = new Chart(ctx, {
@@ -71,9 +85,10 @@ const useBurnRateChart = (initialBalance, expenses, quarters) => {
 const BurnRateCalculator = () => {
   const [initialBalance, setInitialBalance] = useState(0);
   const [expenses, setExpenses] = useState(0);
-  const [quarters, setQuarters] = useState(0);
+  const [period, setPeriod] = useState(0);
+  const [periodType, setPeriodType] = useState('quarters');
 
-  const { updateChart, canvasRef } = useBurnRateChart(initialBalance, expenses, quarters);
+  const { updateChart, canvasRef } = useBurnRateChart(initialBalance, expenses, period, periodType);
 
   return (
     <Container>
@@ -97,31 +112,39 @@ const BurnRateCalculator = () => {
             placeholder="Enter monthly expenses"
           />
         </Form.Group>
-        <Form.Group controlId="quarters">
-          <Form.Label>Number of Quarters</Form.Label>
+        <Form.Group controlId="period">
+          <Form.Label>Number of {periodType === 'quarters' ? 'Quarters' : 'Years'}</Form.Label>
           <Form.Control
             type="number"
-            value={quarters}
-            onChange={(e) => setQuarters(parseInt(e.target.value))}
-            placeholder="Enter number of quarters"
+            value={period}
+            onChange={(e) => setPeriod(parseInt(e.target.value))}
+            placeholder={`Enter number of ${periodType === 'quarters' ? 'quarters' : 'years'}`}
           />
+        </Form.Group>
+        <Form.Group controlId="periodType">
+          <Form.Label>Period Type</Form.Label>
+          <Form.Control as="select" value={periodType} onChange={(e) => setPeriodType(e.target.value)}>
+            <option value="quarters">Quarters</option>
+            <option value="years">Years</option>
+          </Form.Control>
         </Form.Group>
       </Form>
       <Button
         className="mb-4"
         onClick={updateChart}
-        disabled={!initialBalance || !expenses || !quarters} // Add this line
+        disabled={!initialBalance || !expenses || !period}
       >
         Update Chart
       </Button>
       <canvas ref={canvasRef}></canvas>
-      {!initialBalance || !expenses || !quarters && ( // Add this line
+      {!initialBalance || !expenses || !period ? (
         <div className="alert alert-danger" role="alert">
           Please enter values for all fields
         </div>
-      )}
+      ) : null}
     </Container>
   );
 };
 
 export default BurnRateCalculator;
+
