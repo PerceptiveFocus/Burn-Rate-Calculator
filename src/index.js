0314531729313f85d707adc4ac4a3318d70b3870
Perceptive Focus <113016229+PerceptiveFocus@users.noreply.github.com>
2023-03-27 02:23:27 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import BurnRateProjection from './BurnRateProjection';
-import ChartWrapper from './ChartWrapper';
 
 (async () => {
   try {
